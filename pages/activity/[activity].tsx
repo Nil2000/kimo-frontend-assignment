45b353add63b7d8bcd8fd6cd8d99a374fd60a179
nilabhra-adhikari-main <nilabhra09b.net@gmail.com>
2023-04-10 20:11:23 +0530

    new-update

--- pages/activity/[activity].tsx
+++ pages/activity/[activity].tsx
@@ -18,7 +18,7 @@ export const checkEnvironment = () => {
     let base_url =
         process.env.NODE_ENV === "development"
             ? "http://localhost:3000"
-            : "https://example.com"; // https://v2ds.netlify.app
+            : "https://v2ds.netlify.app"; // https://v2ds.netlify.app
 
     return base_url;
 };
